6c61d0ec878ac66fcdf33b8d34f4ff7a0a9bb19a
sumittiwari4527-pro <sumit.tiwari4527@gmail.com>
2026-09-12 18:53:44 +0530

    Use centralized route definitions

--- src/app/RoutingShell.tsx
+++ src/app/RoutingShell.tsx
@@ -1,14 +1,6 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router";
 import App from "./App";
-import { articles } from "./data/articles";
-
-const articleSlug = (title: string) =>
-  title.toLowerCase().replace(/[^a-z0-9]+/g, "-").replace(/^-|-$/g, "");
-
-const articleRoutes = articles.map((article) => ({
-  id: article.id,
-  path: `/blog/${articleSlug(article.title)}`,
-}));
+import { ARTICLE_ROUTES, TOOL_ROUTES } from "./utils/routes";
 
 export default function RoutingShell() {
   return (
@@ -16,12 +8,11 @@ export default function RoutingShell() {
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/tools" element={<App />} />
-        <Route path="/tools/fire-calculator" element={<App />} />
-        <Route path="/tools/goal-planner" element={<App />} />
-        <Route path="/tools/bmi-calculator" element={<App />} />
-        <Route path="/tools/unit-converter" element={<App />} />
+        {Object.values(TOOL_ROUTES).map((path) => (
+          <Route key={path} path={path} element={<App />} />
+        ))}
         <Route path="/blog" element={<App />} />
-        {articleRoutes.map((article) => (
+        {ARTICLE_ROUTES.map((article) => (
           <Route key={article.id} path={article.path} element={<App />} />
         ))}
         <Route path="*" element={<Navigate to="/" replace />} />
